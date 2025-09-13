00fef0d781015e4f72730ae44a9cb2715aaec212
zxieb <zxiebb@connect.ust.hk>
2025-09-13 16:14:33 +0800

    Add 1 print

--- src/main/java/lab1/mainApp1.java
+++ src/main/java/lab1/mainApp1.java
@@ -4,6 +4,7 @@ public class mainApp1 {
     public static void main(String[] args) {
         int b = 2;
         int n = 11;
+        System.out.println("third version");
         System.out.println("Program..starting");
         System.out.println("1+1="+2);
         System.out.println(b+" to power "+n+" = "+ myLibrary.Power(b,n));
